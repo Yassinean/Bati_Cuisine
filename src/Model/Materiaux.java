3aa18cc616d33531a68ab29e307e05855c7d1cf4
Yassinean <yassinedubraska@gmail.com>
2024-09-18 16:56:05 +0100

    Materiaux model

--- src/Model/Materiaux.java
+++ src/Model/Materiaux.java
@@ -1,4 +1,58 @@
 package Model;
 
-public class Materiaux {
+public class Materiaux extends Composant {
+    private double coutUnitaire;
+    private double quantite;
+    private double coutTransport;
+    private double coefficientQualite;
+
+    public Materiaux(int id, String nom, String typeComposant, double tauxTva, double coutUnitaire, double quantite, double coutTransport, double coefficientQualite) {
+        super(id, nom, typeComposant, tauxTva);
+        this.coutUnitaire = coutUnitaire;
+        this.quantite = quantite;
+        this.coutTransport = coutTransport;
+        this.coefficientQualite = coefficientQualite;
+    }
+
+    public double getCoutUnitaire() {
+        return coutUnitaire;
+    }
+
+    public void setCoutUnitaire(double coutUnitaire) {
+        this.coutUnitaire = coutUnitaire;
+    }
+
+    public double getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(double quantite) {
+        this.quantite = quantite;
+    }
+
+    public double getCoutTransport() {
+        return coutTransport;
+    }
+
+    public void setCoutTransport(double coutTransport) {
+        this.coutTransport = coutTransport;
+    }
+
+    public double getCoefficientQualite() {
+        return coefficientQualite;
+    }
+
+    public void setCoefficientQualite(double coefficientQualite) {
+        this.coefficientQualite = coefficientQualite;
+    }
+
+    @Override
+    public String toString() {
+        return "Materiaux =>" +
+                "\nCout Unitaire : " + coutUnitaire +
+                "\nQuantite : " + quantite +
+                "\nCout Transport : " + coutTransport +
+                "\nCoefficient Qualite : " + coefficientQualite +
+                '}';
+    }
 }
