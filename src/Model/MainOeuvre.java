76c9893474cc7223ff81119b9f64605a4998d4a1
Yassinean <yassinedubraska@gmail.com>
2024-09-18 16:56:15 +0100

    MainOeuvre model

--- src/Model/MainOeuvre.java
+++ src/Model/MainOeuvre.java
@@ -1,27 +1,16 @@
 package Model;
 
 public class MainOeuvre extends Composant{
-    private int id;
+
     private double tauxHoraire;
     private double heureTravail;
     private double productiviteOuvrier;
 
     public MainOeuvre(int id, String nom, String typeComposant, double tauxTva, int id1, double tauxHoraire, double heureTravail, double productiviteOuvrier) {
         super(id, nom, typeComposant, tauxTva);
-        this.id = id1;
         this.tauxHoraire = tauxHoraire;
         this.heureTravail = heureTravail;
         this.productiviteOuvrier = productiviteOuvrier;
-    }
-
-    @Override
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(int id) {
-        this.id = id;
     }
 
     public double getTauxHoraire() {
