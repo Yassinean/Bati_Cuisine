43e7fa7db3b592cdf0a4c2df43d4f891495f8d56
Yassinean <yassinedubraska@gmail.com>
2024-09-18 23:22:16 +0100

    projet ReposInterface done

--- src/Repository/Interface/IProjet.java
+++ src/Repository/Interface/IProjet.java
@@ -5,5 +5,7 @@ import Model.Projet;
 import java.util.*;
 
 public interface IProjet {
-
+    List <Projet> findAllProjects();
+    Projet findProjectById(Integer id);
+    void addProject(Projet projet);
 }
