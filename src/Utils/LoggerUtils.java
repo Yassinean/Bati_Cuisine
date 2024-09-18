d23b380578e8fe01cafccf5b09f468c5f7a5644f
Yassinean <yassinedubraska@gmail.com>
2024-09-18 14:59:25 +0100

    adding Utils>LoggerUtils class

---
+++ src/Utils/LoggerUtils.java
@@ -0,0 +1,14 @@
+package Utils;
+
+import java.util.logging.Logger;
+
+public final class LoggerUtils {
+    private static final Logger logger = Logger.getLogger(LoggerUtils.class.getName());
+
+    private LoggerUtils() {
+    }
+
+    public static Logger getLogger() {
+        return getLogger();
+    }
+}
