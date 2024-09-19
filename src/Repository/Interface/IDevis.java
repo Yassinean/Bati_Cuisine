b16d26054d6e360a316d319683822539199780dd
Yassinean <yassinedubraska@gmail.com>
2024-09-19 16:15:28 +0100

    adding devis interface repos

--- src/Repository/Interface/IDevis.java
+++ src/Repository/Interface/IDevis.java
@@ -1,6 +1,12 @@
 package Repository.Interface;
 
-public interface IDevis {
+import Model.Devis;
 
+import java.util.List;
+
+public interface IDevis {
+    List<Devis> findAllDeviss();
+    Devis findDevisById(Integer id);
+    void addDevis(Devis devis);
 
 }
