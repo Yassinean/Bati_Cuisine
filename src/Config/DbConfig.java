0d202444d2d0950c2acdc8d1d0cc06db3780554e
Yassinean <yassinedubraska@gmail.com>
2024-09-23 15:04:43 +0100

    update gitignore && adding .env

---
+++ src/Config/DbConfig.java
@@ -0,0 +1,46 @@
+package Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class DbConfig {
+    private static volatile DbConfig instance;
+    private final Connection connection;
+    private static final Dotenv dotenv = Dotenv.load();
+    String dbPort = dotenv.get("DB_PORT");
+    String dbName = dotenv.get("DB_NAME");
+    String dbUserName = dotenv.get("DB_USERNAME");
+    String dbPassword = dotenv.get("DB_PASSWORD");
+
+    private final String url = "jdbc:postgresql://localhost:" + dbPort + "/" + dbName;
+    private final String username = dbUserName;
+    private final String password = dbPassword;
+
+    private DbConfig() throws SQLException {
+        try {
+            this.connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new SQLException("Impossible de se connecter à la base de données", e);
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static DbConfig getInstance() throws SQLException {
+        if (instance == null) {
+            synchronized (DbConfig.class) {
+                if (instance == null) {
+                    instance = new DbConfig();
+                }
+            }
+        } else if (instance.getConnection().isClosed()) {
+            instance = new DbConfig();
+        }
+        return instance;
+    }
+}
+
